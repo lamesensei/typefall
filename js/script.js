e2abe1ed1b46e025197818cb91e2c88a0a2b92a2
lamesensei <16118606+lamesensei@users.noreply.github.com>
2018-09-14 13:31:38 +0800

    Implement per character checking

--- js/script.js
+++ js/script.js
@@ -7,31 +7,50 @@ var currentWord = ''
 
 window.onload = function() {
 
+    function clear() {
+        currentKeys = [];
+    }
+
     function compareKeys() {
-        if (currentKeys.join('') == currentWord)
-            debuggerDisplay.textContent = 'match'
+        if (currentKeys.length == currentWordLength) {
+            debuggerDisplay.textContent = 'correct'
+            console.log('correct, word reset')
+            clear()
+            displayRandomWord(wordsArray)
+        }
+        if (currentKeys.length != 0) {
+            if (currentKeys.join('')[currentKeys.length - 1] == currentWord[currentKeys.length - 1]) {
+                debuggerDisplay.textContent = `match: ${currentKeys.join('')}`
+            }
+            else {
+                debuggerDisplay.textContent = 'fail'
+                clear()
+                displayRandomWord(wordsArray)
+            }
+        }
+
     }
 
     function detectKeyPress(event) {
         currentKeys.push(event.key)
         console.log(currentKeys)
 
-        debuggerDisplay.textContent = `Current keys: ${currentKeys.join('')}, Press Count: ${currentKeys.length} `
-
-        if (currentKeys.length == currentWordLength)
-            compareKeys()
+        //debuggerDisplay.textContent = `Current keys: ${currentKeys.join('')}, Press Count: ${currentKeys.length} `
+        compareKeys()
     }
 
     function displayRandomWord(words) {
-        var randomIndex = Math.floor(Math.random() * words.length - 1)
+        var randomIndex = Math.floor(Math.random() * (words.length))
+        if (currentWord == words[randomIndex])
+            return displayRandomWord(words)
         currentWord = words[randomIndex]
-        console.log(randomIndex);
+        console.log(`${randomIndex} ${words.length}`)
         wordDisplay.textContent = currentWord
         currentWordLength = words[randomIndex].length
     }
 
     //event listeners
-    window.addEventListener('keyup', detectKeyPress)
+    window.addEventListener('keydown', detectKeyPress)
 
     //function calls
     displayRandomWord(wordsArray)
