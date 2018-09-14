ae4c131f920d76aa23754c5b8a92656936361e0a
lamesensei <16118606+lamesensei@users.noreply.github.com>
2018-09-14 11:32:21 +0800

    Implement basic checking functionality

--- js/script.js
+++ js/script.js
@@ -1,21 +1,38 @@
-var words = ['sick', 'ham', 'mad']
+var wordsArray = ['sick', 'ham', 'mad', 'hakunamatada']
 var wordDisplay = document.getElementById('word-display')
 var debuggerDisplay = document.getElementsByClassName('debug')[0]
 var currentKeys = []
+var currentWordLength = 0
+var currentWord = ''
 
 window.onload = function() {
 
     function compareKeys() {
-
+        if (currentKeys.join('') == currentWord)
+            debuggerDisplay.textContent = 'match'
     }
 
     function detectKeyPress(event) {
         currentKeys.push(event.key)
         console.log(currentKeys)
-        debuggerDisplay.textContent = currentKeys.toString()
+
+        debuggerDisplay.textContent = `Current keys: ${currentKeys.join('')}, Press Count: ${currentKeys.length} `
+
+        if (currentKeys.length == currentWordLength)
+            compareKeys()
     }
 
+    function displayRandomWord(words) {
+        var randomIndex = Math.floor(Math.random() * words.length - 1)
+        currentWord = words[randomIndex]
+        console.log(randomIndex);
+        wordDisplay.textContent = currentWord
+        currentWordLength = words[randomIndex].length
+    }
 
-
+    //event listeners
     window.addEventListener('keyup', detectKeyPress)
+
+    //function calls
+    displayRandomWord(wordsArray)
 }
